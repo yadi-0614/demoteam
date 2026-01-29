184d1e188f990ba1fe45e434af1afc748b050cf2
jimenezgollo430-ui <jimenezgollo430@gmail.com>
2026-01-29 12:26:02 -0600

    Add initial HTML structure and .nvmrc file

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Bienvenidos
+    <br>
+    <h2> Equipo ludika</h2>    
+    <br>
+    <h2> Integrantes</h2>
+    <ul>
+        <li><a href="./daniel.docx"></a> Daniel</li>
+        <li><a href="./jose luis.docx"></a> Jose luis</li>
+        <li><a href="./jimena.docx"></a> Jimena</li>
+        <li><a href="./lucero yadira.docx"></a> Lucero Yadira</li>
+        <li><a href="./francisco.docx"></a> Francisco</li>
+    </ul>        
+</h1>
+</body>
+</html>
